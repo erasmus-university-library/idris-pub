0a1f01523d7ffb1f1f32d0692f2bda9987822a47
Jasper Op de Coul <jasper.opdecoul@eur.nl>
2019-01-14 13:14:07 +0100

    added production defaults for gunicorn

---
+++ api/gunicorn.conf.py
@@ -0,0 +1,8 @@
+import multiprocessing
+
+workers = multiprocessing.cpu_count() * 2 + 2
+forwarded_allow_ips = '*'
+secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
+max_requests = 1024
+max_requests_jitter = 256
+timeout = 60
