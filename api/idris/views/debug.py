0bbf104e3f1e34726a15b53ff7ce8764bb3450c2
Jasper Op de Coul <jasper.opdecoul@eur.nl>
2019-01-14 13:15:19 +0100

    added sqlalchemy pool settings with debug view

--- api/idris/views/debug.py
+++ api/idris/views/debug.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
-
+"""
 @view_config(route_name='debug')
 def echo_view(request):
     response = request.response
@@ -11,15 +11,18 @@ def echo_view(request):
         elif header.startswith('Echo-'):
             response.headers[header[5:]] = request.headers[header]
     return response
-
 """
+
 @view_config(route_name='debug')
 def debug_view(request):
+    pool = request.registry['engine'].pool
+    info = ("Pool size: %d  Connections in pool: %d "\
+            "Current Overflow: %d Current Checked out "\
+            "connections: %d" % (pool.size(),
+                                 pool.checkedin(),
+                                 pool.overflow(),
+                                 pool.checkedout()))
+
     request.response.content_type = 'text/plain'
-    for header in request.headers:
-        request.response.write(
-            ('%s: %s\n' % (header, request.headers[header])).encode('utf8'))
-    request.response.write('\n')
-    request.response.write(request.body)
+    request.response.write(info)
     return request.response
-"""
